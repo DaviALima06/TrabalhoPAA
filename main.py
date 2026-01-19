166b52487de797abb5985ff0a3ca11865d4b932d
DaviALima06 <davialima06092001@gmail.com>
2026-01-19 17:05:52 -0400

    Atualiza README e arquivo principal

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Projeto: TrabalhoPA
+Disciplina: Projeto e Análise de Algoritmos
+Descrição: Implementação de algoritmos utilizando Programação Dinâmica
+Autor: 
+GitHub: https://github.com/DaviALima06
+Data: 2026-01-19
+"""
+
+def main():
+    """
+    Função principal do projeto.
+    Ponto de entrada da aplicação.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
